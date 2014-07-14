5b1358069bd518171c82bb383456e564b3b0ecce
Ramon-Wong <rcnwong78@gmail.com>
2014-07-14 19:29:28 -0300

    Commiting on Version 2

--- functions.h
+++ functions.h
@@ -4,12 +4,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <GL/glew.h>
+#include <GL/glfw.h>
+
 /*
  * stuf stuf stuf stuf stuf stuf 
 */
 
 
 
+extern GLuint		GLSL_Program;
+extern GLuint		GLSL_vertex;
+extern GLuint		GLSL_fragment;
 
+
+void				Init(void);
+void				Shut_Down(int return_code);
+void				Main_Loop(void);
+void				Draw(void);
+
+char			*	ReadFile(const char *);
+void				print_shader_info_log(GLuint);
+void				print_program_info_log(GLuint);
+void				ShaderSetup(void);
 
 #endif
